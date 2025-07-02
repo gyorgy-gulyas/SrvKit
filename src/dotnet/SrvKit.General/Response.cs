ddd7aa6126ef03f6cc4b92368109e70e84890c3f
Gulyás György <gulyas.gyorgy@gmail.com>
2025-07-02 13:05:45 +0200

    WIP

--- src/dotnet/SrvKit.General/Response.cs
+++ src/dotnet/SrvKit.General/Response.cs
@@ -3,6 +3,17 @@ namespace ServiceKit.Net
     public class Response
     {
         public Error Error { get; protected set; } = null;
+
+        public Response()
+        {
+            Error = null;
+        }
+
+        public Response(Error error)
+        {
+            Error = error;
+        }
+
         public bool IsSuccess() => Error == null;
         public static Response Success() => new();
         public static Response Failure(Error error) => new() { Error = error };
@@ -11,9 +22,21 @@ namespace ServiceKit.Net
     public class Response<TValue> : Response
         where TValue : class
     {
+        public Response(TValue Value)
+        {
+            Value = null;
+            Error = null;
+        }
+
+        public Response(Error error)
+            : base(error)
+        {
+            Value = null;
+        }
+
         public TValue Value { get; private set; } = null;
         public bool HasValue() => Value != null;
-        public static Response<TValue> Success(TValue value) => new() { Value = value };
-        public static new Response<TValue> Failure(Error error) => new() { Error = error };
+        public static Response<TValue> Success(TValue value) => new(value);
+        public static new Response<TValue> Failure(Error error) => new(error);
     }
 }
